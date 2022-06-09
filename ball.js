579cc9f045e63c92f5f08eae0a4fb937b04f4a8b
realguystuff <101049350+realguystuff@users.noreply.github.com>
2022-06-09 11:18:50 +0800

    Update ball.js

--- ball.js
+++ ball.js
@@ -20,24 +20,15 @@ class BouncingBall {
   }
 
   update() {
-    let stopped = false;
     const hd = height - this.r / 2;
     this.s *= 0.99;
     this.y += this.s;
     this.s += this.a;
     if (this.y > hd) {
       this.y = hd;
-      if (!stopped) {
-        this.s = -this.s + 1;
-      }
+      this.s = -this.s + 1;
     }
     this.a -= this.friction;
-
-    fill(255);
-    textSize(12);
-    text("speed: " + this.s, 0, height - 10);
-    text("acc: " + this.a, 0, height);
-    text("x: " + this.x + ", y: " + this.y, 0, height - 20);
 
     if (this.debug) {
       const radu = this.y - this.r / 2;
